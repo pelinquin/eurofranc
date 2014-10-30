649b3f2f9d91bc2077c62eb53e232140b71dee9d
Laurent Fournier <pelinquin@gmail.com>
2014-10-30 16:19:20 +0100

    list

--- ef.py
+++ ef.py
@@ -322,11 +322,11 @@ def app_index(d, env):
     return o + footer()
 
 def app_list(d):
-    o = header() + favicon() + style_html() + title() + '<table>'
+    o, un = header() + favicon() + style_html() + title() + '<table>', '<euro>&thinsp;€</euro>'
     dpub, dblc = ropen(d['pub']), ropen(d['blc'])
     for i, src in enumerate(dpub.keys()): 
         blc = int(dblc[src])/100 if src in dblc else '0'
-        o += '<tr><td class="num">%d</td><td class="mono">%s</td><td class="num">%s</td></tr>' % (i+1, btob64(src), blc)
+        o += '<tr><td class="num">%d</td><td class="mono">%s</td><td class="num">%7.2f%s</td></tr>' % (i+1, btob64(src), blc, un)
     dpub.close()
     dblc.close()
     return o + '</table>' + footer()
