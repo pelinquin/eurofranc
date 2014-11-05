68ef8df70965f983c1628a8f9768cf69977d81fd
Laurent Fournier <pelinquin@gmail.com>
2014-11-05 09:19:57 +0100

    testflight

--- ef.py
+++ ef.py
@@ -341,7 +341,7 @@ def header():
     return '<?xml version="1.0" encoding="utf8"?>\n<html>\n<meta name="viewport" content="width=device-width, initial-scale=1"/>'
 
 def title():
-    return '<a href="./"><img title="Eurofranc 2015, pour l\'économie réelle !" src="%s" width="100"/></a>\n' % (get_image('logo.png'))
+    return '<a href="./"><img title="Eurofranc 2015, pour l\'économie réelle !" src="%s" width="48"/></a>\n' % (get_image('logo.png'))
 
 def get_image(img):
     "_"
@@ -367,6 +367,7 @@ def footer():
 
 def app_index(d, env):
     o = header() + favicon() + style_html() + title()
+    o += '<b class="green">Si vous désirez tester gratuitement l\'application <i>iOS</i>, envoyez nous votre e-mail d\'<i>AppleID</i> et installez <a href="https://www.testflightapp.com/>TestFlight</a> sur votre iPhone sous <i>iOS8</i></b><br/>'
     o += '<p><i>Consultez un compte</i><form method="post"><input class="txt" title="code \'MOI\' de 18 caractères alphanumérique affiché en haut du téléphone" pattern="\S+" required="yes" name="cm" placeholder="...€f-ID"/><input title="@nom-public-Twitter ou alias-privé-local" class="txt" pattern=".+" required="yes" name="alias" placeholder="@... (Twitter)"/><input type="submit" value="ok"/></form></p>\n'
     if 'HTTP_COOKIE' in env:
         for x in env['HTTP_COOKIE'].split(';'):
