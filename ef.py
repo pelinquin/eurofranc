5b14ed5939ea1bbabcfcd4d9a32059a8e8081b56
Laurent Fournier <pelinquin@gmail.com>
2014-11-20 11:09:07 +0100

    cup transactions add

--- ef.py
+++ ef.py
@@ -413,14 +413,15 @@ def app_users(d, env):
 def app_trx(d):
     o, un = header() + favicon() + style_html(), '<euro>&thinsp;€</euro>'
     dtrx = ropen(d['trx'])
-    o += '<table><tr><td>%s</td><td class="num">%d transactions</td></tr></table><table>' % (title(), len(dtrx.keys())) 
+    l = len(filter(lambda x:len(x) == 13, dtrx.keys()))
+    o += '<table><tr><td>%s</td><td class="num">%d transactions</td></tr></table><table>' % (title(), l) 
     for i, t in enumerate(filter(lambda x:len(x) == 13, dtrx.keys())):
-    #for i, t in enumerate(dtrx.keys()): 
-        if len(t) == 13:            
+        if len(dtrx[t] == 150): 
             prf = btob64(t[4:])[:1] + btob64(dtrx[t][:9])[:1]
-            o += '<tr><td class="num">%d</td><td class="num">%s</td><td><a href="./%s" class="mono">%s</a></td><td><a href="%s" class="mono">%s</a></td><td class="mono smallgreen">%s%08d</td><td class="num">%7.2f%s</tr>' % (i+1, datdecode(t[:4]), btob64(t[4:]), btob64(t[4:]), btob64(dtrx[t][:9]), btob64(dtrx[t][:9]), prf ,b2i(dtrx[t][11:14]), b2i(dtrx[t][9:11])/100, un)
+            o += '<tr><td class="num">%d</td><td class="num">%s</td><td><a href="./%s" class="mono">%s</a></td><td><a href="%s" class="mono">%s</a></td><td class="mono smallgreen">%s%08d</td><td class="num">%7.2f%s</td></tr>' % (i+1, datdecode(t[:4]), btob64(t[4:]), btob64(t[4:]), btob64(dtrx[t][:9]), btob64(dtrx[t][:9]), prf ,b2i(dtrx[t][11:14]), b2i(dtrx[t][9:11])/100, un)
         else:
-            o += '<tr><td class="num">%d</td><td class="num">%d</td></tr>' % (i+1, len(t))
+            o += '<tr><td class="num">%d</td><td class="num">%s</td><td><a href="./%s" class="mono">%s</a></td></tr>' % (i+1, datdecode(t[:4]), btob64(t[4:]), btob64(t[4:])))
+
     dtrx.close()
     return o + '</table>' + footer()
 
