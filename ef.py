a8d6611913c897d057aab25e6669c7c58392a75a
Laurent Fournier <pelinquin@gmail.com>
2014-12-09 13:16:21 +0100

    if read

--- ef.py
+++ ef.py
@@ -877,10 +877,7 @@ def application(environ, start_response):
         s = raw # use directory or argument
         if re.match('(\S{2,30})$', base) and len(s) == 196: o = buyig(environ, d, b64tob(bytes(s, 'ascii')), base)
         elif re.match('(\S{2,30})$', base) and len(s) == 36: 
-            if readig(environ, b64tob(bytes(s, 'ascii')), base): 
-                o = 'ok ig' #mime = 'application/pdf'
-            else:
-                o += 'ko readig'
+            if o = readig(environ, b64tob(bytes(s, 'ascii')), base): mime = 'application/pdf'
         elif re.match('(\S{2,30})$', base) and s == ':': o = '%d:%d:%d' % curpkn('/%s/%s_%s/igf/%s.igf' % (__app__, __app__, port, base))
         elif re.match('(\S{2,30})$', base) and s == '@': o = igregister(environ, d, base)
         elif re.match('\S{12}$', base): o, mime = app_report(d, base, environ), 'text/html; charset=utf-8'
